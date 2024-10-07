7e7a4739919f290cc070319f1881fbb31d9f16b2
ledxdeliveryflopp <korstim18@gmail.com>
2024-10-07 20:04:51 +0700

    add decode token func, add user id in token payload

--- src/authorization/repository.py
+++ src/authorization/repository.py
@@ -6,6 +6,7 @@ from sqlalchemy import Select
 from src.authorization.models import TokenModel
 from src.authorization.schemas import LoginSchemas
 from src.authorization.utils import create_token
+from src.settings.exceptions import UserDontExist
 from src.settings.service import BaseService
 from src.user.models import UserModel
 
@@ -21,7 +22,11 @@ class AuthorizationRepository(BaseService):
 
     async def __repository_create_access_token(self, email: str) -> dict:
         """Сохранение токена"""
-        token_data = await create_token(email)
+        user = await self._repository_find_user_by_email(email)
+        if not user:
+            raise UserDontExist
+        user_id = user.id
+        token_data = await create_token(email, user_id)
         token = TokenModel(token=token_data.get("token"), expire=token_data.get("expire"))
         await self.service_save_object(token)
         return token_data
--- src/authorization/utils.py
+++ src/authorization/utils.py
@@ -13,14 +13,23 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 @logger.catch
-async def create_token(email: str) -> dict:
+async def create_token(email: str, user_id: int) -> dict:
     """Создание токена"""
     expire_data = datetime.utcnow() + timedelta(minutes=15)
     random_string: str = random.choices(string.printable, k=random.randint(5, 15))
-    token_payload: dict = {"user_email": email, "random_str": random_string}
+    token_payload: dict = {"user_email": email, "user_id": user_id, "random_str": random_string}
     jwt_token = jwt.encode(token_payload, settings.jwt_settings.jwt_secret,
                            settings.jwt_settings.jwt_algorithm)
     return {"token": jwt_token, "expire": expire_data}
+
+
+@logger.catch
+async def decode_token_data(jwt_token: str) -> dict:
+    """Дешифровка токена"""
+    payload_data = jwt.decode(jwt_token, settings.jwt_settings.jwt_secret,
+                              settings.jwt_settings.jwt_algorithm)
+    user_id = payload_data.get("user_id")
+    return {"user_id": user_id}
 
 
 @logger.catch
